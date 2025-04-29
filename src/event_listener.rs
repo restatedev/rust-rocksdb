2605c127121728f53958bed3af461ee54445c3e0
Pavel Tcholakov <pavel@restate.dev>
2025-04-29 16:25:33 +0200

    Add TablePropertiesCollector support
    This change the ability to configure custom TablePropertiesCollector
    factories, and to access user-collected properties via the FlushJobInfo
    event.

--- src/event_listener.rs
+++ src/event_listener.rs
@@ -12,6 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::ffi::{CStr, CString};
+use std::marker::PhantomData;
+use std::ptr::NonNull;
 use std::sync::Arc;
 
 use libc::c_void;
@@ -94,8 +97,8 @@ impl EventListenerExt for Options {
                 Some(EventListenerCallback::<T>::on_flush_completed),
             );
 
-            // Takes ownership of the event listener, which will also drop the
-            // EventListenerCallback via the destructor callback
+            // Takes ownership of the event listener; the Rust callback wrapper will be
+            // dropped via the destructor callback
             ffi::rocksdb_options_add_event_listener(self.inner, event_listener_ptr);
         }
     }
@@ -103,7 +106,7 @@ impl EventListenerExt for Options {
 
 /// Flush information
 #[derive(Debug)]
-pub struct FlushJobInfo {
+pub struct FlushJobInfo<'a> {
     /// The name of the column family
     pub cf_name: String,
     /// The path to the newly created file
@@ -114,6 +117,12 @@ pub struct FlushJobInfo {
     pub largest_seqno: u64,
     /// Flush reason
     pub flush_reason: FlushReason,
+
+    // holds a pointer to data borrowed from RocksDB for the duration of the event handler callback;
+    // we are responsible for freeing the wrapper struct
+    table_properties: NonNull<ffi::rocksdb_table_properties_t>,
+
+    _marker: PhantomData<&'a ()>,
 }
 
 /// Flush reason
@@ -135,6 +144,34 @@ pub enum FlushReason {
     ErrorRecoveryRetryFlush = 12,
     WalFull = 13,
     CatchUpAfterErrorRecovery = 14,
+}
+
+impl Drop for FlushJobInfo<'_> {
+    fn drop(&mut self) {
+        unsafe {
+            ffi::rocksdb_table_properties_destroy(self.table_properties.as_ptr());
+        }
+    }
+}
+
+impl<'a> FlushJobInfo<'a> {
+    pub fn get_user_collected_property(&self, key: &str) -> Option<String> {
+        unsafe {
+            let key_cstring = CString::new(key).unwrap();
+            let value_ptr = ffi::rocksdb_table_properties_get_user_collected_property(
+                self.table_properties.as_ptr(),
+                key_cstring.as_ptr(),
+            );
+
+            if value_ptr.is_null() {
+                return None;
+            };
+
+            let value_string = CStr::from_ptr(value_ptr).to_str().unwrap().to_owned();
+            ffi::rocksdb_free(value_ptr as *mut c_void);
+            Some(value_string)
+        }
+    }
 }
 
 pub(crate) struct EventListenerCallback<T>
@@ -180,12 +217,19 @@ where
             ffi::rocksdb_flushjobinfo_flushreason(flush_job_info),
         );
 
+        let table_properties = NonNull::new(unsafe {
+            ffi::rocksdb_flushjobinfo_table_properties(flush_job_info).cast_mut()
+        })
+        .unwrap();
+
         let flush_job_info = FlushJobInfo {
             cf_name,
             file_path,
             smallest_seqno,
             largest_seqno,
             flush_reason,
+            table_properties,
+            _marker: PhantomData,
         };
 
         let cb = &mut *(raw_cb as *mut Self);
---
+++ tests/test_table_properties.rs
@@ -0,0 +1,146 @@
+mod util;
+
+use std::collections::HashMap;
+use std::ffi::{CStr, CString};
+use std::sync::Arc;
+
+use parking_lot::RwLock;
+
+use rust_rocksdb::event_listener::{EventListener, EventListenerExt, FlushJobInfo};
+use rust_rocksdb::table_properties::{
+    EntryType, TablePropertiesCollector, TablePropertiesCollectorContext,
+    TablePropertiesCollectorFactory, TablePropertiesExt,
+};
+use rust_rocksdb::{Options, DB};
+use util::DBPath;
+
+#[test]
+fn test_table_properties_collector() {
+    let path = DBPath::new("_rust_rocksdb_properties_collector_test");
+
+    let mut opts = Options::default();
+    opts.create_if_missing(true);
+    opts.create_missing_column_families(true);
+
+    // Contrary to what the docs say, event listeners are DB-level, and can't be set at the CF level
+    let listener = Arc::new(CustomPropertyListener {
+        state: RwLock::new(ObservedProperties::default()),
+    });
+    opts.add_event_listener(listener.clone());
+
+    let mut cf_opts = Options::default();
+    cf_opts.add_table_properties_collector_factory(KeyStartsWithACollectorFactory(
+        CString::new("AKeyCounterFactory").unwrap(),
+    ));
+
+    let db = DB::open_cf_with_opts(&opts, &path, [("cf", cf_opts)]).unwrap();
+
+    let cf = db.cf_handle("cf").unwrap();
+    db.put_cf(&cf, b"a", b"foo").unwrap();
+    assert_eq!(0, listener.state.read().flush_count);
+    assert_eq!(None, listener.state.read().latest_custom_property_value);
+
+    db.flush_cf(&cf).unwrap();
+    assert_eq!(1, listener.state.read().flush_count);
+    assert_eq!(
+        Some("1".to_owned()),
+        listener.state.read().latest_custom_property_value
+    );
+
+    db.put_cf(&cf, b"aaa", b"foo").unwrap();
+    db.put_cf(&cf, b"bbb", b"bar").unwrap();
+    db.put_cf(&cf, b"AAA", b"baz").unwrap();
+
+    assert_eq!(1, listener.state.read().flush_count);
+    assert_eq!(
+        Some("1".to_owned()),
+        listener.state.read().latest_custom_property_value
+    );
+
+    db.flush_cf(&cf).unwrap();
+    assert_eq!(2, listener.state.read().flush_count);
+    assert_eq!(
+        Some("2".to_owned()),
+        listener.state.read().latest_custom_property_value
+    );
+
+    db.put_cf(&cf, b"c", b"").unwrap();
+    db.flush_cf(&cf).unwrap();
+    assert_eq!(3, listener.state.read().flush_count);
+    assert_eq!(
+        Some("0".to_owned()),
+        listener.state.read().latest_custom_property_value
+    );
+}
+
+struct KeyStartsWithACollectorFactory(CString);
+
+impl TablePropertiesCollectorFactory for KeyStartsWithACollectorFactory {
+    type Collector = KeyStartsWithACollector;
+
+    fn create(&mut self, _context: TablePropertiesCollectorContext) -> Self::Collector {
+        KeyStartsWithACollector {
+            name: CString::new("AKeyCounter").unwrap(),
+            count: 0,
+        }
+    }
+
+    fn name(&self) -> &CStr {
+        &self.0
+    }
+}
+
+#[derive(Debug)]
+struct KeyStartsWithACollector {
+    name: CString,
+    count: usize,
+}
+
+impl TablePropertiesCollector for KeyStartsWithACollector {
+    fn add_user_key(
+        &mut self,
+        key: &[u8],
+        _value: &[u8],
+        entry_type: EntryType,
+        _seq: u64,
+        _file_size: u64,
+    ) -> bool {
+        if let EntryType::EntryPut = entry_type {
+            if key.starts_with(b"a") || key.starts_with(b"A") {
+                self.count += 1;
+            }
+        }
+        true
+    }
+
+    fn finish(&mut self, properties: &mut HashMap<String, String>) -> bool {
+        properties.insert("key_count".to_owned(), self.count.to_string());
+        true
+    }
+
+    fn get_readable_properties(&self) -> HashMap<String, String> {
+        HashMap::new()
+    }
+
+    fn name(&self) -> &CStr {
+        &self.name
+    }
+}
+
+struct CustomPropertyListener {
+    state: RwLock<ObservedProperties>,
+}
+
+#[derive(Default)]
+struct ObservedProperties {
+    flush_count: u32,
+    latest_custom_property_value: Option<String>,
+}
+
+impl EventListener for CustomPropertyListener {
+    fn on_flush_completed(&self, info: FlushJobInfo) {
+        let mut guard = self.state.write();
+        guard.flush_count += 1;
+        guard.latest_custom_property_value = info.get_user_collected_property("key_count");
+    }
+}
