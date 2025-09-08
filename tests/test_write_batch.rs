e9d861ee8513141708de9a73949378e75eb94431
Ahmed Farghal <AhmedSoliman@users.noreply.github.com>
2025-09-08 12:24:34 +0300

    Adds support for vectored put/merge operations in WriteBatch (#15)

--- tests/test_write_batch.rs
+++ tests/test_write_batch.rs
@@ -17,7 +17,8 @@ use std::collections::HashMap;
 
 use pretty_assertions::assert_eq;
 
-use rust_rocksdb::{DB, Error, WriteBatch, WriteBatchIterator, WriteBatchIteratorCf};
+use rust_rocksdb::{DB, Error, WriteBatch, WriteBatchIterator, WriteBatchIteratorCf, ColumnFamilyDescriptor, Options, };
+use std::io::IoSlice;
 use util::DBPath;
 
 #[test]
@@ -149,4 +150,177 @@ fn test_write_batch_put_log_data() {
     }
 
     assert!(called);
+}
+
+#[test]
+fn test_write_batch_put_cf_vectored() {
+    let path = DBPath::new("writebatch_put_cf_vectored");
+    let db = DB::open_default(&path).unwrap();
+
+    let mut batch = WriteBatch::default();
+
+    let key_part1 = b"hello";
+    let key_part2 = b"_world";
+    let value_part1 = b"foo";
+    let value_part2 = b"_bar";
+
+    let key_slices = [IoSlice::new(key_part1), IoSlice::new(key_part2)];
+    let value_slices = [IoSlice::new(value_part1), IoSlice::new(value_part2)];
+
+    batch.put_vectored(&key_slices, &value_slices);
+
+    let result = db.write(&batch);
+    assert!(result.is_ok());
+
+    let retrieved = db.get(b"hello_world").unwrap();
+    assert_eq!(retrieved.unwrap(), b"foo_bar");
+}
+
+#[test]
+fn test_write_batch_put_vectored() {
+    let path = DBPath::new("writebatch_put_vectored");
+    let mut opts = Options::default();
+    opts.create_if_missing(true);
+    opts.create_missing_column_families(true);
+
+    let cf_descriptor = ColumnFamilyDescriptor::new("test_cf", Options::default());
+    let db = DB::open_cf_descriptors(&opts, &path, vec![cf_descriptor]).unwrap();
+
+    let cf = db.cf_handle("test_cf").unwrap();
+
+    let mut batch = WriteBatch::default();
+
+    let key_part1 = b"hello";
+    let key_part2 = b"_world";
+    let value_part1 = b"foo";
+    let value_part2 = b"_bar";
+
+    let key_slices = [IoSlice::new(key_part1), IoSlice::new(key_part2)];
+    let value_slices = [IoSlice::new(value_part1), IoSlice::new(value_part2)];
+
+    batch.put_cf_vectored(&cf, &key_slices, &value_slices);
+
+    let result = db.write(&batch);
+    assert!(result.is_ok());
+
+    let retrieved = db.get_cf(&cf, b"hello_world").unwrap();
+    assert_eq!(retrieved.unwrap(), b"foo_bar");
+}
+
+#[test]
+fn test_write_batch_merge_cf_vectored() {
+    fn test_merge_operator(
+        _new_key: &[u8],
+        existing_val: Option<&[u8]>,
+        operands: &rust_rocksdb::MergeOperands,
+    ) -> Option<Vec<u8>> {
+        let mut result: Vec<u8> = Vec::new();
+        if let Some(v) = existing_val {
+            result.extend_from_slice(v);
+        }
+        for op in operands {
+            result.extend_from_slice(op);
+        }
+        Some(result)
+    }
+
+    let path = DBPath::new("writebatch_merge_cf_vectored");
+    let mut opts = Options::default();
+    opts.create_if_missing(true);
+    opts.create_missing_column_families(true);
+    opts.set_merge_operator_associative("test_merge", test_merge_operator);
+
+    let mut cf_opts = Options::default();
+    cf_opts.set_merge_operator_associative("test_merge", test_merge_operator);
+    let cf_descriptor = ColumnFamilyDescriptor::new("test_cf", cf_opts);
+    let db = DB::open_cf_descriptors(&opts, &path, vec![cf_descriptor]).unwrap();
+
+    let cf = db.cf_handle("test_cf").unwrap();
+
+    let mut batch = WriteBatch::default();
+
+    let key_part1 = b"merge";
+    let key_part2 = b"_key";
+    let value_part1 = b"val";
+    let value_part2 = b"ue";
+
+    let key_slices = [IoSlice::new(key_part1), IoSlice::new(key_part2)];
+    let value_slices = [IoSlice::new(value_part1), IoSlice::new(value_part2)];
+
+    batch.put_cf(&cf, b"merge_key", b"initial");
+    batch.merge_cf_vectored(&cf, &key_slices, &value_slices);
+
+    let result = db.write(&batch);
+    assert!(result.is_ok());
+
+    let retrieved = db.get_cf(&cf, b"merge_key").unwrap();
+    assert_eq!(retrieved.unwrap(), b"initialvalue");
+}
+
+#[test]
+fn test_write_batch_put_vectored_no_cf() {
+    let path = DBPath::new("writebatch_put_vectored_no_cf");
+    let db = DB::open_default(&path).unwrap();
+
+    let mut batch = WriteBatch::default();
+
+    let key_part1 = b"hello";
+    let key_part2 = b"_world";
+    let value_part1 = b"foo";
+    let value_part2 = b"_bar";
+
+    let key_slices = [IoSlice::new(key_part1), IoSlice::new(key_part2)];
+    let value_slices = [IoSlice::new(value_part1), IoSlice::new(value_part2)];
+
+    batch.put_vectored(&key_slices, &value_slices);
+
+    let result = db.write(&batch);
+    assert!(result.is_ok());
+
+    let retrieved = db.get(b"hello_world").unwrap();
+    assert_eq!(retrieved.unwrap(), b"foo_bar");
+}
+
+#[test]
+fn test_write_batch_merge_vectored_no_cf() {
+    fn test_merge_operator(
+        _new_key: &[u8],
+        existing_val: Option<&[u8]>,
+        operands: &rust_rocksdb::MergeOperands,
+    ) -> Option<Vec<u8>> {
+        let mut result: Vec<u8> = Vec::new();
+        if let Some(v) = existing_val {
+            result.extend_from_slice(v);
+        }
+        for op in operands {
+            result.extend_from_slice(op);
+        }
+        Some(result)
+    }
+
+    let path = DBPath::new("writebatch_merge_vectored_no_cf");
+    let mut opts = Options::default();
+    opts.create_if_missing(true);
+    opts.set_merge_operator_associative("test_merge", test_merge_operator);
+
+    let db = DB::open(&opts, &path).unwrap();
+
+    let mut batch = WriteBatch::default();
+
+    let key_part1 = b"merge";
+    let key_part2 = b"_key";
+    let value_part1 = b"val";
+    let value_part2 = b"ue";
+
+    let key_slices = [IoSlice::new(key_part1), IoSlice::new(key_part2)];
+    let value_slices = [IoSlice::new(value_part1), IoSlice::new(value_part2)];
+
+    batch.put(b"merge_key", b"initial");
+    batch.merge_vectored(&key_slices, &value_slices);
+
+    let result = db.write(&batch);
+    assert!(result.is_ok());
+
+    let retrieved = db.get(b"merge_key").unwrap();
+    assert_eq!(retrieved.unwrap(), b"initialvalue");
 }
